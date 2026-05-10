0663f961c36414c965fcfddffdc84297e73bc15a
Jon DeVree <nuxi@vault24.org>
2026-05-09 20:21:13 -0400

    Changes for diff algorithm

--- foo.java
+++ foo.java
@@ -1,20 +1,24 @@
 public class File1 {
 
-  public int add (int a, int b)
-  {
-    log();
-    return a + b;
-  }
-
   public int sub (int a, int b)
   {
-    if (a == b)
+    // TOOD: JIRA1234
+    if ( isNull(a, b) )
     {
-        return 0;
+        return null
     }
     log();
     return a - b;
-    // TOOD: JIRA1234
+  }
+
+  public int mul (int a, int b)
+  {
+    if ( isNull(a, b) )
+    {
+        return null;
+    }
+    log();
+    return a * b;
   }
 
 }
